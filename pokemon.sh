1826e36a33ec2e85b6827b1eec6533458659a09a
RafaMadera18 <112293466+RafaMadera18@users.noreply.github.com>
2025-02-17 16:11:08 -0600

    Script V1

---
+++ pokemon.sh
@@ -0,0 +1,40 @@
+if [ -z "$1" ]; then
+  echo "Error: No se proporcionó un argumento."
+  exit 1
+fi
+
+POKEMON=$1
+API="https://pokeapi.co/api/v2/pokemon/$POKEMON"
+CSV="pokemon.csv"
+
+touch "$CSV"
+
+RESPONSE=$(curl -s "$API")
+
+
+if [ -z "$RESPONSE" ] || [ "$(echo "$RESPONSE" | jq -r '.id // empty')" = "" ]; then
+  echo "El Pokémon '$POKEMON' no existe."
+  exit 1
+fi
+
+ID=$(echo "$RESPONSE" | jq -r '.id')
+NAME=$(echo "$RESPONSE" | jq -r '.name')
+WEIGHT=$(echo "$RESPONSE" | jq -r '.weight')
+HEIGHT=$(echo "$RESPONSE" | jq -r '.height')
+ORDER=$(echo "$RESPONSE" | jq -r '.order')
+
+echo "${NAME^} (No. $ID)"
+echo "Id = $ID"
+echo "Weight = $WEIGHT"
+echo "Height = $HEIGHT"
+echo "Order = $ORDER"
+
+echo "Guardando en $CSV..."
+
+if [ ! -s "$CSV" ]; then
+  echo "id,name,weight,height,order" > "$CSV"
+fi
+
+echo "$ID,$NAME,$WEIGHT,$HEIGHT,$ORDER" >> "$CSV"
+
+echo "Datos guardados correctamente."
